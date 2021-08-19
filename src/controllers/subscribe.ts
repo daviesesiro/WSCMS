c51ebdbc2d3d7e678d4051f0a2d4c4b4204e908d
daviesesiro <ojurereoluwad@gmail.com>
2021-08-19 13:02:40 +0100

    update

--- src/controllers/subscribe.ts
+++ src/controllers/subscribe.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { Subscription } from "../models/Subscription";
-import webpush from "../services/webpush";
 
 export const SubscribeController = async (req: Request, res: Response) => {
   //get push subscription object from the request
@@ -8,14 +7,6 @@ export const SubscribeController = async (req: Request, res: Response) => {
 
   //send status 201 for the request
   res.status(201).json({});
-
-  //create paylod: specified the detals of the push notification
-  const payload = JSON.stringify({ title: "You have subscribed to WMCMS" });
-
-  //pass the object into sendNotification fucntion and catch any error
-  webpush
-    .sendNotification(subscription, payload)
-    .catch((err) => console.error(err));
 
   await Subscription.create({
     subscription,
